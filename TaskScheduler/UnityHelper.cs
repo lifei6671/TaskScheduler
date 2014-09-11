a494ad4c1db18cea95fd51fb0456ee75d3ca4b62
lifei6671 <longfei6671@163.com>
2014-09-11 16:32:05 +0800

    添加注释

--- TaskScheduler/UnityHelper.cs
+++ TaskScheduler/UnityHelper.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
 
@@ -13,19 +10,26 @@ namespace TaskScheduler
     /// </summary>
     public class UnityHelper
     {
+        //静态依赖注入对象
         private static readonly IUnityContainer UnityContainer = new UnityContainer();
+        /// <summary>
+        /// 获取依赖注入对象
+        /// </summary>
         public static IUnityContainer GetUnityContainer { get { return UnityContainer; } }
         /// <summary>
         /// 注册类型
         /// </summary>
-        public static void Register()
+        public static void Register() 
         {
+            //获取依赖注入对象
             IUnityContainer unityContainer = UnityContainer;
-
+            //初始化配置文件路径
             ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap { ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + "Unity.config" };
+            //读取配置文件
             Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            //获取配置节点
             UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection("unity");
-
+            //加载配置数据
             unityContainer.LoadConfiguration(section);
 
         }
